4648dd3859d2a4d4a08a6e3d0f41f592bb36f278
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-06-20 12:30:53 +1000

    added DeviceInfoService to main index.ts

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var title_service_1 = require("./peek-util/title.service");
 exports.TitleService = title_service_1.TitleService;
+var device_info_interface_1 = require("./peek-util/services/device-info.interface");
+exports.DeviceInfoService = device_info_interface_1.DeviceInfoService;
 //# sourceMappingURL=/home/peek/project/peek-util/index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export {TitleService, TitleBarLink} from "./peek-util/title.service";
+export {DeviceInfoService} from "./peek-util/services/device-info.interface"
