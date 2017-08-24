ade18721653e83fa6a3bd0ebb7731e307323c486
Brenton Ford <brenton.ford@synerty.com>
2017-08-24 12:12:28 -0400

    corrected typo "Cannot find module './peek-util/platform/Platfrom'."

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var title_service_1 = require("./peek-util/title.service");
 exports.TitleService = title_service_1.TitleService;
-var Platfrom_1 = require("./peek-util/platform/Platfrom");
-exports.DeviceSizeE = Platfrom_1.DeviceSizeE;
+var Platform_1 = require("./peek-util/platform/Platform");
+exports.DeviceSizeE = Platform_1.DeviceSizeE;
 //# sourceMappingURL=/Users/peek/peek-dev/peek-util/index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export {TitleService, TitleBarLink} from "./peek-util/title.service";
-export {DeviceSizeE} from "./peek-util/platform/Platfrom";
+export {DeviceSizeE} from "./peek-util/platform/Platform";
