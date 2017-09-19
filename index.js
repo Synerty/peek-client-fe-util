6a7b6e122afaa49ac79e59c0accf2e0472a01a10
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-09-19 15:09:56 +1000

    Updated index

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var title_service_1 = require("./peek-util/title.service");
 exports.TitleService = title_service_1.TitleService;
+var footer_service_1 = require("./peek-util/footer.service");
+exports.FooterService = footer_service_1.FooterService;
+var nav_back_service_1 = require("./peek-util/nav-back.service");
+exports.NavBackService = nav_back_service_1.NavBackService;
 var Platform_1 = require("./peek-util/platform/Platform");
 exports.DeviceSizeE = Platform_1.DeviceSizeE;
 //# sourceMappingURL=/home/peek/project/peek-util/index.js.map
